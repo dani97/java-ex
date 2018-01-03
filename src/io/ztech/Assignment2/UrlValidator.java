4f91e2cb1f955727269c513e27955a4f0f44daf3
dani97 <christopherdaniel13397@gmail.com>
2018-01-04 00:17:45 +0530

    second excercise html tags

--- src/io/ztech/Assignment2/UrlValidator.java
+++ src/io/ztech/Assignment2/UrlValidator.java
@@ -8,6 +8,10 @@ public class UrlValidator {
 	public static boolean validateUrl(String url) {
 		return url.matches("[a-zA-Z]+://(www.)?[a-zA-Z0-9]+[.][a-zA-Z]+$");
 	}
+	
+	public static String replaceHtmlTags(String html) {
+		return html.replaceAll("<[a-zA-Z -//]+>", "");
+	}
 
 	public static void main(String[] args) {
 		Logger logger = Logger.getLogger(UrlValidator.class.getName());
@@ -15,6 +19,8 @@ public class UrlValidator {
 		Scanner scanner = new Scanner(System.in);
 		String url = scanner.nextLine();
 		logger.log(Level.INFO, Boolean.toString(validateUrl(url)));
+		String html = scanner.nextLine();
+		logger.log(Level.INFO, replaceHtmlTags(html));
 		scanner.close();
 	}
 }
