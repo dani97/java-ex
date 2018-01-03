15bdf42b0c8f5818b4a7b9660d555403508e2634
dani97 <christopherdaniel13397@gmail.com>
2018-01-04 00:02:26 +0530

    second excercise url validator

---
+++ src/io/ztech/Assignment2/UrlValidator.java
@@ -0,0 +1,19 @@
+package io.ztech.Assignment2;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UrlValidator {
+	public static  boolean validateUrl(String url) {
+		return url.matches("[a-zA-Z]+://(www.)?[a-zA-Z0-9]+[.][a-zA-Z]+$");
+	}
+	public static void main(String [] args) {
+		Logger logger = Logger.getLogger(UrlValidator.class.getName());
+		logger.log(Level.INFO,"enter url");
+		Scanner scanner = new Scanner(System.in);
+		String url = scanner.nextLine();
+		logger.log(Level.INFO,Boolean.toString(validateUrl(url)));
+		scanner.close();
+	}
+}
